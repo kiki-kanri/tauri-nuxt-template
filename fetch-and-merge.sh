f7d542d1b525f535729fbfa39e451d9de6a65b0f
kiki-kanri <a470666@gmail.com>
2026-06-11 10:54:54 +0800

    chore: update scripts

--- fetch-and-merge.sh
+++ fetch-and-merge.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
-git fetch https://github.com/kiki-kanri/rs-crates-template main
+git fetch https://github.com/kiki-kanri/tauri-nuxt-template main
 git merge FETCH_HEAD
